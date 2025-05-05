99eea8dd6edd56235c708ef07c42b91d0a1a8223
kaichik96 <dulaternazar772@gmail.com>
2025-05-05 11:15:24 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.dirname(__file__)))
+
+from app import create_app
+
+app = create_app()
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
